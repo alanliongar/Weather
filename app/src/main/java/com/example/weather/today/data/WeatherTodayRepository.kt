2face83a06a275efd4059e03e29b7261eef4c52f
alanliongar <alan.lucindo.gomes@alumni.usp.br>
2025-07-29 09:36:42 -0300

    Repository pattern partially implemented

--- app/src/main/java/com/example/weather/today/data/WeatherTodayRepository.kt
+++ app/src/main/java/com/example/weather/today/data/WeatherTodayRepository.kt
@@ -1,6 +1,7 @@
 package com.example.weather.today.data
 
 import com.example.weather.today.data.local.WeatherTodayLocalDataSource
+import com.example.weather.today.data.model.WeatherTodayDTO
 import com.example.weather.today.data.remote.WeatherTodayRemoteDataSource
 
 class WeatherTodayRepository(
@@ -8,4 +9,15 @@ class WeatherTodayRepository(
     private val remote: WeatherTodayRemoteDataSource
 ) {
 
+    suspend fun getWeatherToday(
+        latitude: Float,
+        longitude: Float,
+        forecastDays: Int = 2
+    ): Result<WeatherTodayDTO?> {
+        return remote.getWeatherToday(
+            latitude = latitude,
+            longitude = longitude,
+            forecastDays = forecastDays
+        )
+    }
 }
